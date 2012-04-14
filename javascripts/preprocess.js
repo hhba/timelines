8e443291dd727950e9e6ec8e6713b1ba66130414
Felipe Lerena <felipelerena@gmail.com>
2012-04-14 16:16:42 -0300

    agregando funcion de merge

--- javascripts/preprocess.js
+++ javascripts/preprocess.js
@@ -32,3 +32,28 @@ function preprocessData(data) {
 
 	return {eventList: eventList, groups: groups};
 }
+
+function merge_segments(events, places_position){
+    var result = Array();
+    for(event in events){
+        var pos_x = 0;
+        var pos_y = 0;
+        var character = events[event];
+        var positions = character['position'];
+        var character_time = {'segments': []};
+        var prev = null;
+        for (position in positions){
+            pos_y = places_position[positions[position]['group']];
+            pos_x = positions[position]['orderBox'];
+            var current = [pos_x, pos_y]
+            character_time['segments'].push({'start': current});
+            if(prev != null) {
+                positions[position - 1]['end'] = current;
+            }
+            prev = current;
+        }
+        character_time['name'] = character['name'];
+        result.push(character_time);
+    }
+    return result;
+}
